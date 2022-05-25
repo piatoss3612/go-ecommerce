fe914aab2d02af13cf37a1d8cc8aca118d8fc10d
Hyohwak Lee <crewe1746@naver.com>
2022-05-25 16:16:57 +0900

    finish up handler and rename card package to cards

--- go-stripe/cmd/api/handlers-api.go
+++ go-stripe/cmd/api/handlers-api.go
@@ -2,31 +2,74 @@ package main
 
 import (
 	"encoding/json"
+	"myapp/internal/cards"
 	"net/http"
+	"strconv"
 )
 
-type stripPayload struct {
+type stripePayload struct {
 	Currency string `json:"currency"`
 	Amount   string `json:"amount"`
 }
 
 type jsonResponse struct {
 	OK      bool   `json:"ok"`
-	Message string `json:"message"`
-	Content string `json:"content"`
-	ID      int    `json:"id"`
+	Message string `json:"message,omitempty"`
+	Content string `json:"content,omitempty"`
+	ID      int    `json:"id,omitempty"`
 }
 
 func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
-	j := jsonResponse{
-		OK: true,
-	}
+	var payload stripePayload
 
-	out, err := json.MarshalIndent(j, "", "\t")
+	err := json.NewDecoder(r.Body).Decode(&payload) // decode r.Body
 	if err != nil {
 		app.errorLog.Println(err)
+		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(out)
+	amount, err := strconv.Atoi(payload.Amount)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	// create Card instance
+	card := cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: payload.Currency,
+	}
+
+	okay := true
+
+	pi, msg, err := card.Charge(payload.Currency, amount) // try to charge
+	if err != nil {
+		okay = false
+	}
+
+	// succeeded
+	if okay {
+		out, err := json.MarshalIndent(pi, "", "\t")
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	} else { // failed
+		j := jsonResponse{
+			OK:      false,
+			Message: msg,
+			Content: "",
+		}
+
+		out, err := json.MarshalIndent(j, "", "\t")
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	}
 }
