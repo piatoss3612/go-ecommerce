668f7aa07ccff63604cf9834aa4fb4eb0135e5c5
Hyohwak Lee <crewe1746@naver.com>
2022-05-26 14:39:48 +0900

    Create product page

--- go-stripe/cmd/web/handlers.go
+++ go-stripe/cmd/web/handlers.go
@@ -37,3 +37,10 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 		app.errorLog.Println(err)
 	}
 }
+
+// displays the page to buy on widget
+func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "buy-once", nil); err != nil {
+		app.errorLog.Println(err)
+	}
+}
