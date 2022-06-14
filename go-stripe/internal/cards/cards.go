06e5c3a75e7a8559ef5a62f7c255181668604f90
Hyohwak Lee <crewe1746@naver.com>
2022-06-14 13:22:04 +0900

    Create Refund method on cards package

--- go-stripe/internal/cards/cards.go
+++ go-stripe/internal/cards/cards.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stripe/stripe-go/v72/customer"
 	"github.com/stripe/stripe-go/v72/paymentintent"
 	"github.com/stripe/stripe-go/v72/paymentmethod"
+	"github.com/stripe/stripe-go/v72/refund"
 	"github.com/stripe/stripe-go/v72/sub"
 )
 
@@ -123,6 +124,25 @@ func (c *Card) SubscribeToPlan(cust *stripe.Customer, plan, email, last4, cardTy
 	}
 
 	return subscription, nil
+}
+
+// get a refund
+func (c *Card) Refund(pi string, amount int) error {
+	stripe.Key = c.Secret
+
+	amountToRefund := int64(amount)
+
+	refundParams := &stripe.RefundParams{
+		Amount:        &amountToRefund,
+		PaymentIntent: &pi,
+	}
+
+	_, err := refund.New(refundParams)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // custom error messages
